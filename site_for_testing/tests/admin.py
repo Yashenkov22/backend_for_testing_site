637c1fcd5cf6df5fee2dfc30fc5ee1d66dbb0dc9
Vlad <yashenkov.v@yandex.ru>
2023-06-18 17:56:32 +0300

    save results in session

--- site_for_testing/tests/admin.py
+++ site_for_testing/tests/admin.py
@@ -5,14 +5,23 @@ from .models import *
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', )
 
+
 @admin.register(Test)
 class TestAdmin(admin.ModelAdmin):
     list_display = ('name', 'level', 'category', 'created_at', 'updated_at')
 
+
+class AnswerInLine(admin.TabularInline):
+    model = Answer
+    extra = 4
+
+
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question', 'test')
+    inlines = [AnswerInLine]
+
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
-    list_display = ('answer', 'is_correct', 'question')
+    list_display = ('answer', 'is_correct')
--- site_for_testing/tests/views.py
+++ site_for_testing/tests/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from .models import Category, Test, Question
@@ -33,22 +33,30 @@ def question_page(request: HttpRequest, cat_id, test_id, quest_id):
     if request.method == 'POST':
         answer = request.POST.get(f'question{quest_id}')
         request.session['answers'][f'question{quest_id}'] = answer
+        request.session.modified = True
+        print(request.session['answers'])
+        quest_id += 1
         try:
-            question = Question.objects.get(pk=quest_id+1)
+            question = Question.objects.get(pk=quest_id)
         except ObjectDoesNotExist:
             test = Test.objects.get(pk=test_id)
             answers = list(request.session['answers'].values())
             correct = len([ans for ans in answers if ans == 'True'])
             uncorrect = len(answers) - correct
+            procents = round(correct / len(answers), 2) * 100
             context = {
                 'test': test,
                 'answers': answers,
                 'correct': correct,
                 'uncorrect': uncorrect,
+                'procents': procents,
             }
             return render(request, 'tests/test_results.html', context=context)
+        else:
+            return redirect(question.get_absolute_url())
     else:
         if quest_id == 1:
             request.session['answers'] = {}
+        print(request.session['answers'])
         question = Question.objects.get(pk=quest_id)
     return render(request, 'tests/question_page.html', context={'question': question})
